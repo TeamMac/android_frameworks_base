b592cb48985a294e78dd78c4138c492b970cde3a
Dhananjay Bhardwaj <worldallaroundme@gmail.com>
2012-09-28 08:58:05 +0530

    Update telephony/java/com/android/internal/telephony/RIL.java
    Add manual network selection 6/6

--- telephony/java/com/android/internal/telephony/RIL.java
+++ telephony/java/com/android/internal/telephony/RIL.java
@@ -1576,6 +1576,21 @@ public class RIL extends BaseCommands implements CommandsInterface {
     }
 
     public void
+    setNetworkSelectionModeManual(String operatorNumeric, String operatorRAT, Message response) {
+    	RILRequest rr
+    	        = RILRequest.obtain(RIL_REQUEST_SET_NETWORK_SELECTION_MANUAL,
+    	                            response);
+    	                            
+    	if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest)
+    	            + " " + operatorNumeric);
+    	           
+    	rr.mp.writeString(operatorNumeric);
+    	rr.mp.writeString(operatorRAT);
+    	
+    	send(rr);
+    }
+    	
+    public void
     getNetworkSelectionMode(Message response) {
         RILRequest rr
                 = RILRequest.obtain(RIL_REQUEST_QUERY_NETWORK_SELECTION_MODE,
@@ -3242,7 +3257,8 @@ public class RIL extends BaseCommands implements CommandsInterface {
                     strings[i+0],
                     strings[i+1],
                     strings[i+2],
-                    strings[i+3]));
+                    strings[i+3],
+                    strings[i+4]));
         }
 
         return ret;
